dab4544968c35124d9fbd230390b933c174058e0
Mateo Di Loreto <mateoadiloreto@gmail.com>
2023-09-12 00:02:23 -0300

    new datalake deployments

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~>3.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.15.0"
+    }
   }
   backend "azurerm" {
       resource_group_name  = "tfstate"
@@ -18,27 +22,79 @@ provider "azurerm" {
   features {}
 }
 
-#Resource Group de Ejemplo
+# Configure the Azure Active Directory Provider
+provider "azuread" {
+  tenant_id = "88940f07-5551-49e6-8453-c578e8759aaf"
+}
 
-# resource "azurerm_resource_group" "state-demo-secure-rg" {
-#   name     = "state-demo"
-#   location = "eastus"
-# }
 
-#START> Function App Python 3.10 con Consumption plan <
-
-resource "azurerm_resource_group" "functionapp-py-consumption-rg" {
-  name     = local.functionapp_rg_name
+#>Datalake<
+resource "azurerm_resource_group" "DataLake_lab" {
+  name     = "MB-DataLake_Lab-rg"
   location = "East US 2"
 }
 
-resource "azurerm_storage_account" "functionapp-py-consumption-sa" {
-  name                     = local.functionapp_sa_name
-  resource_group_name      = azurerm_resource_group.functionapp-py-consumption-rg.name
-  location                 = azurerm_resource_group.functionapp-py-consumption-rg.location
+resource "azurerm_storage_account" "DataLake_lab-sa" {
+  name                     = "mbdatalakelabsa01"
+  resource_group_name      = azurerm_resource_group.DataLake_lab.name
+  location                 = azurerm_resource_group.DataLake_lab.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  is_hns_enabled           = "true"
 }
+
+resource "azurerm_storage_container" "bronze" {
+  name                  = "bronze"
+  storage_account_name  = azurerm_storage_account.DataLake_lab-sa.name
+  container_access_type = "private"
+  depends_on = [ azurerm_storage_account.DataLake_lab-sa ]
+}
+
+resource "azurerm_role_assignment" "example" {
+  principal_id   = "5e193d65-8a83-453f-a1ef-393e94e25053"
+  role_definition_name = "Storage Blob Data Contributor"
+  scope          = azurerm_storage_account.DataLake_lab-sa.id
+}
+
+
+resource "azurerm_storage_data_lake_gen2_filesystem" "fileSystem01" {
+  name               = "gold"
+  storage_account_id = azurerm_storage_account.DataLake_lab-sa.id
+
+  properties = {
+    hello = "aGVsbG8="
+  }
+}
+
+resource "azurerm_storage_data_lake_gen2_path" "directory01" {
+  path               = "root/sub01/sub02"
+  filesystem_name    = azurerm_storage_data_lake_gen2_filesystem.fileSystem01.name
+  storage_account_id = azurerm_storage_account.DataLake_lab-sa.id
+  resource           = "directory"
+}
+
+#END >Datalake<
+
+
+resource "azuread_user" "example" {
+  count               = 32
+  user_principal_name = "user${count.index + 1}@madsblog.onmicrosoft.com"
+  display_name        = "User ${count.index + 1}"
+  password            = "M@dsBlog_2023!"
+}
+# resource "azurerm_resource_group" "functionapp-py-consumption-rg" {
+#   name     = local.functionapp_rg_name
+#   location = "East US 2"
+# }
+
+# resource "azurerm_storage_account" "functionapp-py-consumption-sa" {
+#   name                     = local.functionapp_sa_name
+#   resource_group_name      = azurerm_resource_group.functionapp-py-consumption-rg.name
+#   location                 = azurerm_resource_group.functionapp-py-consumption-rg.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+# }
 
 #azurerm_app_service_plan fue deprecado y sustituido por azurerm_service_plan 
 
@@ -61,60 +117,60 @@ resource "azurerm_storage_account" "functionapp-py-consumption-sa" {
 #   }
 # }
 
-resource "azurerm_service_plan" "functionapp-py-consumption-asp" {
-  name                = local.azure_functions_asp_name
-  resource_group_name = azurerm_resource_group.functionapp-py-consumption-rg.name
-  location            = azurerm_resource_group.functionapp-py-consumption-rg.location
-  os_type             = "Linux"
-  sku_name            = "B1"
-}
+# resource "azurerm_service_plan" "functionapp-py-consumption-asp" {
+#   name                = local.azure_functions_asp_name
+#   resource_group_name = azurerm_resource_group.functionapp-py-consumption-rg.name
+#   location            = azurerm_resource_group.functionapp-py-consumption-rg.location
+#   os_type             = "Linux"
+#   sku_name            = "B1"
+# }
 
-resource "azurerm_linux_function_app" "functionapp-py-consumption-fa" {
-  name                = local.functionapp_name
-  resource_group_name = azurerm_resource_group.functionapp-py-consumption-rg.name
-  location            = azurerm_resource_group.functionapp-py-consumption-rg.location
+# resource "azurerm_linux_function_app" "functionapp-py-consumption-fa" {
+#   name                = local.functionapp_name
+#   resource_group_name = azurerm_resource_group.functionapp-py-consumption-rg.name
+#   location            = azurerm_resource_group.functionapp-py-consumption-rg.location
 
-  storage_account_name       = azurerm_storage_account.functionapp-py-consumption-sa.name
-  storage_account_access_key = azurerm_storage_account.functionapp-py-consumption-sa.primary_access_key
-  service_plan_id            = azurerm_service_plan.functionapp-py-consumption-asp.id
-
-  app_settings = {
-    FUNCTIONS_WORKER_RUNTIME = "python"
-  }
-
-  site_config {
-
-    application_stack {
-      python_version = "3.10"
-    }  
-    cors {
-      allowed_origins = [
-        "https://portal.azure.com",
-      ]
-      support_credentials = false
-    }  
-  
-  }
-}
-#azurerm_function_app fue deprecado y sustituido por azurerm_linux_function_app y azurerm_windows_function_app 
-
-# resource "azurerm_function_app" "functionapp-py-consumption-fa" {
-#   name                       = local.functionapp_name
-#   location                   = azurerm_resource_group.functionapp-py-consumption-rg.location
-#   resource_group_name        = azurerm_resource_group.functionapp-py-consumption-rg.name
-#   app_service_plan_id        = azurerm_service_plan.functionapp-py-consumption-asp.id
 #   storage_account_name       = azurerm_storage_account.functionapp-py-consumption-sa.name
 #   storage_account_access_key = azurerm_storage_account.functionapp-py-consumption-sa.primary_access_key
-#   os_type                    = "linux"
-#   version                    = "~4"
+#   service_plan_id            = azurerm_service_plan.functionapp-py-consumption-asp.id
 
 #   app_settings = {
 #     FUNCTIONS_WORKER_RUNTIME = "python"
 #   }
 
 #   site_config {
-#     linux_fx_version = "python|3.10"
+
+#     application_stack {
+#       python_version = "3.10"
+#     }  
+#     cors {
+#       allowed_origins = [
+#         "https://portal.azure.com",
+#       ]
+#       support_credentials = false
+#     }  
+  
 #   }
 # }
+# #azurerm_function_app fue deprecado y sustituido por azurerm_linux_function_app y azurerm_windows_function_app 
 
-#END > Function App Python 3.10 con Consumption plan <
+# # resource "azurerm_function_app" "functionapp-py-consumption-fa" {
+# #   name                       = local.functionapp_name
+# #   location                   = azurerm_resource_group.functionapp-py-consumption-rg.location
+# #   resource_group_name        = azurerm_resource_group.functionapp-py-consumption-rg.name
+# #   app_service_plan_id        = azurerm_service_plan.functionapp-py-consumption-asp.id
+# #   storage_account_name       = azurerm_storage_account.functionapp-py-consumption-sa.name
+# #   storage_account_access_key = azurerm_storage_account.functionapp-py-consumption-sa.primary_access_key
+# #   os_type                    = "linux"
+# #   version                    = "~4"
+
+# #   app_settings = {
+# #     FUNCTIONS_WORKER_RUNTIME = "python"
+# #   }
+
+# #   site_config {
+# #     linux_fx_version = "python|3.10"
+# #   }
+# # }
+
+# #END > Function App Python 3.10 con Consumption plan <
