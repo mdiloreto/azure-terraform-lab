bf5fc965a7216711ce2ad15a8cae56c0dc0dd456
Mateo Di Loreto <mateoadiloreto@gmail.com>
2023-10-21 18:31:28 -0300

    ubuntu module update

--- Modules/ubuntu20-04_vm/ubuntu.tf
+++ Modules/ubuntu20-04_vm/ubuntu.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "madsblog" {
   name     = var.rg
   location = var.location
 }
@@ -7,12 +7,12 @@ resource "azurerm_virtual_network" "main" {
   name                = "${var.vmname}-network"
   address_space       = ["10.0.0.0/16"]
   location            = var.location
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.madsblog.name
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
+  resource_group_name  = azurerm_resource_group.madsblog.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
 }
@@ -20,7 +20,7 @@ resource "azurerm_subnet" "internal" {
 resource "azurerm_network_interface" "vmnic01" {
   name                = "${var.vmname}-nic01"
   location            = var.location
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.madsblog.name
 
   ip_configuration {
     name                          = "testconfiguration1"
@@ -36,7 +36,7 @@ resource "azurerm_virtual_machine" "vm" {
   network_interface_ids = [
       azurerm_network_interface.vmnic01.id,
   ]
-  resource_group_name   = azurerm_resource_group.example.name
+  resource_group_name   = azurerm_resource_group.madsblog.name
   tags                  = {}
   vm_size               = "Standard_E2s_v3"
   zones                 = [
